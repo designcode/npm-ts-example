5b3021515308cf84dd8001d0b7698cd256f38309
Abdullah Ibrahim <hello@designcode.me>
2022-05-29 20:33:35 +0200

    1.0.1

--- src/add.ts
+++ src/add.ts
@@ -1,1 +1,2 @@
-export const add = (...args: Array<number>): number => args.reduce((acc, number) => acc + number, 0);
+export const add = (...args: Array<number>): number =>
+	args.reduce((acc, number) => acc + number, 0);
